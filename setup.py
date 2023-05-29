e2086e2c295a410a32ac3eff00f61de4a1f51a3f
Soon Siang <jymchng@hotmail.com>
2023-05-29 17:10:41 +0800

    bump version 1.0.2 -> 1.0.3

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='sosecrets_core',
-    version="1.0.2",
+    version="1.0.3",
     description='Simple Secret Primitive for Python',
     author='Jim Chng',
     author_email='jimchng@outlook.com',
